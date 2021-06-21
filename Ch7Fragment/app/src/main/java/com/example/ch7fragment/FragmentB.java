f5f36c2a6d9043fbd4700dfac5d398d456ed001e
kappleword <kappleword@gmail.com>
2021-06-21 17:24:08 +0900

    Fragment로 화면구성

---
+++ Ch7Fragment/app/src/main/java/com/example/ch7fragment/FragmentB.java
@@ -0,0 +1,20 @@
+package com.example.ch7fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentB extends Fragment {
+    //xml 레이아웃이 존재하는 경우 반드시 필요한 메소드
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+}
---
+++ Ch7Fragment/app/src/main/java/com/example/ch7fragment/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.ch7fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Fragment fragment = new FragmentB();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
+        //ft.commitAllowingStateLoss();
+       // ft.commitNow();
+       // ft.commitNowAllowingStateLoss();
+    }
+}
