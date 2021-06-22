c8748e65e61cd50fd29ae9f18285c949f0c5f0d5
kappleword <kappleword@gmail.com>
2021-06-23 00:10:08 +0900

    Fragment 2개 화면에 띄우기연습

---
+++ AndroidStudioProjects/Ch7Quiz/app/src/main/java/com/example/ch7quiz/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ch7quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void setFrag1(View v){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.content_frame, new Fragment1(), "one");
+        ft.commitAllowingStateLoss();
+    }
+    public void setFrag2(View v){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.content_frame, new Fragment2(),"two");
+        ft.commitAllowingStateLoss();
+    }
+}
